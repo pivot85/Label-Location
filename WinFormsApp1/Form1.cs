1d9cc18e1013e6d9da9d8efa92bcedbbbb5b6a1a
pivot85 <82463053+pivot85@users.noreply.github.com>
2021-04-26 17:50:45 +0300

    Add files via upload

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Location = new Point(92,50);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Location = new Point(170,63);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Location = new Point(92, 112);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Location = new Point(50, 63);
+        }
+    }
+}
